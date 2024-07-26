ab59af53ff9912f749b42051908c5befc60b4888
i.protsenko <i.protsenko@quantumobile.com>
2024-07-26 14:41:49 +0300

    FIX

--- read_huge_file/read_huge_file.py
+++ read_huge_file/read_huge_file.py
@@ -3,8 +3,5 @@ from typing import Generator
 
 def read_large_file(file_path: str) -> Generator[str, None, None]:
     with open(file_path, "r") as file:
-        while True:
-            data = file.read(1024 * 1024)
-            if not data:
-                break
-            yield data
+        for line in file:
+            yield line.strip()
